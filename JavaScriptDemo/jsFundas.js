aafd04853fd9da4328c78d36bc20dea9050de9b2
PorkodiVenkatesh <porkodi.venkatesh@revature.com>
2022-07-14 10:56:37 +0530

    upload

--- JavaScriptDemo/jsFundas.js
+++ JavaScriptDemo/jsFundas.js
@@ -40,3 +40,64 @@ console.log(z)
 
 let x = null;
 console.log(x)
+
+//arrays
+let nums = [1, 2, 3, 4, 5];
+let names = ['porkodi', 'rahul', 'avi', 'ais', 'reetika']
+
+console.log(nums.length, names.length)
+
+for (let i = 0; i < nums.length; i++) {
+    console.log(i + "->" + nums[i])
+}
+
+for (let i in names) {
+    console.log(i + "-> " + names[i])
+}
+
+var person = {
+    name: "Porkodi",
+    lastname: "Venkatesh",
+    location: "Chennai"
+}
+
+console.log(typeof(person))
+console.log(person.name);
+person.age = 25;
+console.log(person.age);
+person.location = "Pune"
+console.log(JSON.stringify(person))
+
+//JSON -> JavaScript object Notation
+//key and value 
+var student = [{
+        "id": 1,
+        "name": "rohit",
+        "class": 12
+    },
+    {
+        "id": 2,
+        "name": "shruti",
+        "class": 13
+    }
+]
+
+for (let i in student) {
+    console.log(i, student[i].id, student[i].name, student[i].class)
+}
+
+console.log(student)
+
+//0, false, null, undefined, NaN, '' -> falsy 
+//1, true, 2 3 43, -1 , -90 any other -> truthy
+if ('') {
+    console.log("true block")
+} else {
+    console.log("false block")
+}
+
+if (14 > 8) {
+    console.log("true block")
+} else {
+    console.log("false block")
+}
