c616d8fbf5719086eceb36bd3c3dce7daa25478d
PorkodiVenkatesh <porkodi.venkatesh@revature.com>
2022-07-19 16:03:11 +0530

    directive

---
+++ Angular Projects/directivesDemo/src/app/app.component.ts
@@ -0,0 +1,39 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'directivesDemo';
+
+   nums = [1, 2, 3, 4 ,5]
+   users = [
+    {
+      "id" : "1",
+      "name" : "porkodi"
+    },
+    {
+      "id" : "2",
+      "name" : "sayali"
+    }
+   ]
+
+   emps : emp[] = [
+    { id : 12 , name : "Porkodi" , dept : "Technology"},
+    {id : 13, name : "karthick" , dept: "HR"},
+    {id : 14, name:  "Meera" , dept : "PD"}
+   ];
+
+   num = 0;
+
+
+}
+interface emp {
+  id : number;
+  name : string;
+  dept : string;
+}
+
+
